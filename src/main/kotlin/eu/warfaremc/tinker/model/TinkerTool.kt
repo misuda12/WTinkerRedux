6daea6df7f41c27acda708b3667d5a7238657b3f
WattMann <martin.prejsa1@gmail.com>
2021-01-16 15:44:35 +0100

    Created TinkerTool model

--- src/main/kotlin/eu/warfaremc/tinker/model/TinkerTool.kt
+++ src/main/kotlin/eu/warfaremc/tinker/model/TinkerTool.kt
@@ -1,29 +1,113 @@
-/*
- * This file is part of WarfareMC, licensed under the MIT License.
- *
- * Copyright (C) 2020 WarfareMC & Team
- *
- * Permission is hereby granted, free of charge,
- * to any person obtaining a copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE AUTHORS
- * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 package eu.warfaremc.tinker.model
 
+import eu.warfaremc.tinker.tinker
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
 
-class TinkerTool (val item: ItemStack){
+class TinkerTool private constructor(val item: ItemStack) {
+    companion object {
+
+        private const val LEGACY_TOOL_IDENTIFIER = "&dTinker Tool"
+        private const val LEGACY_BROKEN_TOOL_IDENTIFIER = "&4BROKEN TOOL"
+
+        @JvmStatic
+        fun isTinkerTool(item: ItemStack): Boolean {
+            return isLegacy(item) ||
+                    (item.hasItemMeta() &&
+                            item.itemMeta!!.persistentDataContainer.has(
+                                NamespacedKey(tinker, "level"),
+                                PersistentDataType.INTEGER
+                            ))
+        }
+
+        @JvmStatic
+        fun isRepairMaterial(tool: TinkerTool, type: Material): Boolean {
+            val repairMat: Material = when (tool.item.type) {
+                Material.BOW, Material.WOODEN_SWORD, Material.WOODEN_PICKAXE, Material.WOODEN_AXE, Material.WOODEN_SHOVEL -> Material.OAK_WOOD
+                Material.STONE_SWORD, Material.STONE_PICKAXE, Material.STONE_AXE, Material.STONE_SHOVEL -> Material.STONE
+                Material.IRON_SWORD, Material.IRON_PICKAXE, Material.IRON_AXE, Material.IRON_SHOVEL -> Material.IRON_BLOCK
+                Material.DIAMOND_SWORD, Material.DIAMOND_PICKAXE, Material.DIAMOND_AXE, Material.DIAMOND_SHOVEL -> Material.DIAMOND_BLOCK
+                else -> return false
+            }
+            return repairMat == type
+        }
+
+
+        fun fixLegacy(item: ItemStack): ItemStack? {
+            TODO()
+        }
+
+        @JvmStatic
+        fun isLegacy(item: ItemStack): Boolean {
+            return if (item.hasItemMeta() && item.itemMeta!!.hasLore())
+                item.itemMeta!!.lore!![0].contains(LEGACY_TOOL_IDENTIFIER) ||
+                        item.itemMeta!!.lore!![0].contains(LEGACY_BROKEN_TOOL_IDENTIFIER)
+            else
+                false
+        }
+
+        @JvmStatic
+        fun of(item: ItemStack): TinkerTool? {
+            val fixed: ItemStack
+            if (isTinkerTool(item)) {
+                fixed = if (isLegacy(item))
+                    fixLegacy(item) ?: return null
+                else
+                    item
+                return TinkerTool(fixed)
+            }
+            return null
+        }
+
+    }
+
+    var experience: Int?
+        get() = this.get("experience", PersistentDataType.INTEGER)
+        set(value) = this.set("experience", PersistentDataType.INTEGER, value).also { updateLore() }
+
+    var durability: Int?
+        get() = this.get("durability", PersistentDataType.INTEGER)
+        set(value) = this.set("durability", PersistentDataType.INTEGER, value).also { updateLore() }
+
+    var level: Int?
+        get() = this.get("level", PersistentDataType.INTEGER)
+        set(value) = this.set("level", PersistentDataType.INTEGER, value).also { updateLore() }
+
+    var modificationSpace: Int?
+        get() = this.get("modificationSpace", PersistentDataType.INTEGER)
+        set(value) = this.set("modificationSpace", PersistentDataType.INTEGER, value).also { updateLore() }
+
+    var renamed: Boolean
+        get() = (this.get("renamed", PersistentDataType.INTEGER) ?: 0) > 0
+        set(value) = this.set("renamed", PersistentDataType.INTEGER, value.intValue).also { updateLore() }
+
+
+    private val Boolean.intValue
+        get() = if (this) 1 else 0
+    //TODO("Move to another file")
+
+    private fun updateLore() {
+        TODO()
+    }
+
+    private fun <T, Z> get(key: String?, type: PersistentDataType<T, Z>): Z? {
+        if (key == null)
+            return null
+        if (!this.item.hasItemMeta())
+            return null
+
+        return this.item.itemMeta!!.persistentDataContainer.get(NamespacedKey(tinker, key), type)
+    }
+
+    private fun <T, Z> set(key: String?, type: PersistentDataType<T, Z>, value: Z) {
+        if (key == null)
+            return
+        if (!this.item.hasItemMeta())
+            return
+
+        this.item.itemMeta!!.persistentDataContainer.set(NamespacedKey(tinker, key), type, value)
+    }
 
 }
