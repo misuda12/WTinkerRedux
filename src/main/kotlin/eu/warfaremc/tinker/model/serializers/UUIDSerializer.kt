65e46f2b5f1d6e8df80ad103fab30a1f2e51bd25
WattMann <martin.prejsa1@gmail.com>
2021-02-01 07:16:02 +0100

    CBOR serialization implementation

--- src/main/kotlin/eu/warfaremc/tinker/model/serializers/UUIDSerializer.kt
+++ src/main/kotlin/eu/warfaremc/tinker/model/serializers/UUIDSerializer.kt
@@ -1,6 +1,6 @@
 package eu.warfaremc.tinker.model.serializers
 
-import kotlinx.serialization.KSerializer
+import kotlinx.serialization.*
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
@@ -9,10 +9,12 @@ import kotlinx.serialization.encoding.Encoder
 import java.util.*
 
 object UUIDSerializer : KSerializer<UUID> {
-    override val descriptor: SerialDescriptor
-        get() = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING)
 
-    override fun deserialize(decoder: Decoder): UUID = UUID.fromString(decoder.decodeString())
+    override val descriptor: SerialDescriptor
+        get() = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING);
+
+    override fun deserialize(decoder: Decoder): UUID
+            = UUID.fromString(decoder.decodeString())
 
     override fun serialize(encoder: Encoder, value: UUID) {
         encoder.encodeString(value.toString())
